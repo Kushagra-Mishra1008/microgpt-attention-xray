30b444ff9e8428f28b179d71e611a3fb90a3d93f
kushagra Mishra <mkush526@gmail.com>
2026-07-26 14:34:01 +0530

    Use env var for API base URL, defaulting to localhost

--- web/src/api.js
+++ web/src/api.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://127.0.0.1:8080";
+const API_BASE = import.meta.env.VITE_API_BASE || "http://127.0.0.1:8080";
 
 export async function getModels() {
   const res = await fetch(`${API_BASE}/models`);
